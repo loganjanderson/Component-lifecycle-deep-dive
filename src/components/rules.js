85169fb6d4ff8d66ea77c82b9bcc3d5c1ccfcca1
Logan Anderson <logananderson@Logans-MBP.T-mobile.com>
2021-03-16 14:49:47 -0600

    finished with project for now

---
+++ src/components/rules.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+export default class Rules extends Component {
+    render() {
+        return (
+            <div>
+                Rules...
+            </div>
+        );
+    }
+}
